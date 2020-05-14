3be9368903dc07f81aeb35be70c458137672dfb9
Sourav Purohit <sourav.purohit@tesco.com>
2020-05-14 21:33:10 +0530

    abc

---
+++ src/test/java/com/hackathon/prm/PersonRestaurantManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.prm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonRestaurantManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
