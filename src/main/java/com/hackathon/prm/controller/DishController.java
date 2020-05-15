f07ea9ebbb1cd0157744331a4e61e740ede07d96
Sourav Purohit <sourav.purohit@tesco.com>
2020-05-15 21:40:07 +0530

    swagger implemented

--- src/main/java/com/hackathon/prm/controller/DishController.java
+++ src/main/java/com/hackathon/prm/controller/DishController.java
@@ -1,5 +1,8 @@
 package com.hackathon.prm.controller;
 
+import com.hackathon.prm.dtos.DishDto;
+import com.hackathon.prm.services.DishService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,23 +10,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hackathon.prm.dtos.DishDto;
-import com.hackathon.prm.services.DishService;
-
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 @RestController
 @RequestMapping("v1/prm/expandDish")
 public class DishController {
 
-	@Autowired
-	private DishService dishService;
+    @Autowired
+    private DishService dishService;
 
-	@PostMapping
-	public ResponseEntity<?> login(@RequestBody DishDto dishDto) {
-		log.info("find dish for userid {}, in sourceid {}, in restaurantid {} and dishid {}", dishDto.getUserId(),
-				dishDto.getSourceId(), dishDto.getRestaurantId(), dishDto.getDishId());
-		return ResponseEntity.ok(dishService.getDish(dishDto));
-	}
+    @PostMapping
+    public ResponseEntity<?> login(@RequestBody DishDto dishDto) {
+        log.info("find dish for userid {}, in sourceid {}, in restaurantid {} and dishid {}", dishDto.getUserId(),
+                dishDto.getSourceId(), dishDto.getRestaurantId(), dishDto.getDishId());
+        return ResponseEntity.ok(dishService.getDish(dishDto));
+    }
 }
