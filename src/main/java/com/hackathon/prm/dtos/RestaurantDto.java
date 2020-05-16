5262a868d71626602073ad6670c6f3feca17557d
Vinamra <vinamra.singhal@tesco.com>
2020-05-16 11:40:06 +0530

    restaurant color code update

---
+++ src/main/java/com/hackathon/prm/dtos/RestaurantDto.java
@@ -0,0 +1,20 @@
+package com.hackathon.prm.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RestaurantDto {
+
+	private String id;
+	private int colorCode;
+
+}
---
+++ src/main/java/com/hackathon/prm/repositories/RestaurantRepository.java
@@ -0,0 +1,39 @@
+package com.hackathon.prm.repositories;
+
+import com.hackathon.prm.entities.RestaurantEntity;
+
+import java.util.Optional;
+
+import org.springframework.data.elasticsearch.annotations.Query;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends ElasticsearchRepository<RestaurantEntity, String> {
+	
+	 @Query("{\n" + 
+	 		"  \"bool\": {\n" + 
+	 		"    \"must\": [\n" + 
+	 		"      {\n" + 
+	 		"        \"match\": {\n" + 
+	 		"          \"userId\": \"?0\"\n" + 
+	 		"        }\n" + 
+	 		"      },\n" + 
+	 		"      {\n" + 
+	 		"        \"match\": {\n" + 
+	 		"          \"restaurantId\": \"?1\"\n" + 
+	 		"        }\n" + 
+	 		"      },\n" + 
+	 		"      {\n" + 
+	 		"        \"match\": {\n" + 
+	 		"         \"sourceId\": \"?2\"\n" + 
+	 		"        }\n" + 
+	 		"      }\n" + 
+	 		"    ]\n" + 
+	 		"  }\n" + 
+	 		"}")
+	 RestaurantEntity findByDishDtoCustomQuery(String userId, String restaurantId, String sourceId);
+	 
+	 Optional<RestaurantEntity> findById(String Id);
+	 
+}
